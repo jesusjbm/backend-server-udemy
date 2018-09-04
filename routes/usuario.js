130c21c00189298aa8477d499436fdf7044b2333
ASUS_Src <jesusj.briceno@outlook.es>
2018-09-04 02:47:08 +0200

    Corrección en el error mongoose

--- routes/usuario.js
+++ routes/usuario.js
@@ -7,26 +7,20 @@ var Usuario = require('../models/usuario');
 app.get('/', (req, res, next) => {
 
     Usuario.find({}, 'nombre email img role')
-        .exec(
-            (err, usuarios) => {
-                if (err) {
-                    return res.status(500).json({
-                        ok: false,
-                        mensaje: 'Error cargando usuarios',
-                        errors: err
-                    });
-                }
-
-                return res.status(200).json({
-                    ok: true,
-                    usuarios: usuarios
+        .exec((err, usuarios) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    mensaje: 'Error cargando usuarios',
+                    errors: err
                 });
-            })
+            }
 
-    res.status(500).json({
-        ok: true,
-        mensaje: 'Get usuarios'
-    });
+            return res.status(200).json({
+                ok: true,
+                usuarios: usuarios
+            });
+        })
 });
 
 module.exports = app;
